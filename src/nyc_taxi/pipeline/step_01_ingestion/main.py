f30d7a37884720b235f9886c3ea3d55b2e9249bc
Claresta <claresta080@gmail.com>
2026-03-27 07:31:19 +0700

    feat: implement prefect orchestration and structured logging for ingestion

--- src/nyc_taxi/pipeline/step_01_ingestion/main.py
+++ src/nyc_taxi/pipeline/step_01_ingestion/main.py
@@ -3,8 +3,48 @@ from sodapy import Socrata
 import pandas as pd
 from nyc_taxi.utils.duckdb import save_to_raw
 from nyc_taxi.config.settings import TARGET_DATASETS, DOMAIN, START_DATE, END_DATE
+from prefect import task, flow, get_run_logger
 
-def ingest_taxi_data(*, dataset_id: str, table_name: str, time_col: str):
+# --- TASKS ---
+
+@task(retries=3, retry_delay_seconds=30, name="Fetch Batch From Socrata")
+def fetch_batch(*, client: Socrata, dataset_id: str, table_name: str, time_col: str, offset: int):
+    """Function for fetch data from API"""
+    logger = get_run_logger()
+    logger.info(f"Mengambil data dari offset: {offset}")
+
+    # Tarik potongan data
+    results = client.get(
+        dataset_id,
+        where=f"{time_col} BETWEEN '{START_DATE}' AND '{END_DATE}'",
+        limit=settings.DEFAULT_LIMIT,
+        offset=offset,
+        # order=f"{time_col} ASC" # supaya urut
+    )
+    return results
+        
+@task(name="Save Batch to DuckDB")
+def save_batch(results, table_name):
+    """Function for convert to Pandas and save to DB"""
+    if not results:
+        return 0
+    
+    logger = get_run_logger()
+    
+    # Ubah ke Pandas & Simpan ke DuckDB
+    try:
+        df = pd.DataFrame.from_records(results)
+        save_to_raw(df, table_name=table_name)
+        return len(df)
+    except Exception as e:
+        logger.error(f"❌ Gagal menyimpan ke {table_name}: {e}")
+        return 0
+
+# --- FLOWS ---
+@flow(name="Ingest Per Taxi Type")        
+def ingest_taxi_data_flow(dataset_id: str, table_name: str, time_col:str):
+    logger = get_run_logger()
+    """Sub Flow for one type of taxi"""
     client = Socrata(
         settings.DOMAIN,
         settings.APP_TOKEN,
@@ -15,44 +55,29 @@ def ingest_taxi_data(*, dataset_id: str, table_name: str, time_col: str):
     current_offset = 0
     total_pulled = 0
 
-    print("🚀 Memulai proses Ingestion Masif (Jan-Apr 2023)...")
+    logger.info("🚀 Memulai proses Ingestion Masif (Jan-Apr 2023)...")
 
-    for i in range(2): # ini cuma coba aja, real nya pakai while
+    for _ in range(2): # ini cuma coba aja, real nya pakai while
     # while True:
-        try:
-            # Tarik potongan data
-            results = client.get(
-                dataset_id,
-                where=f"{time_col} BETWEEN '{START_DATE}' AND '{END_DATE}'",
-                limit=settings.DEFAULT_LIMIT,
-                offset=current_offset,
-                # order=f"{time_col} ASC" # supaya urut
-            )
+        results = fetch_batch(client=client, dataset_id=dataset_id, table_name=table_name, time_col=time_col, offset=current_offset)
+            
+        if not results:
+            logger.info(f"🏁 {table_name} selesai ditarik!")
+            break
 
-            if not results:
-                print("🏁 Semua data sudah berhasil ditarik!")
-                break
+        count = save_batch(results, table_name)
 
-            # Ubah ke Pandas & Simpan ke DuckDB
-            df = pd.DataFrame.from_records(results)
-            save_to_raw(df, table_name=table_name)
+        current_offset += count
+        total_pulled += count
+        logger.info(f"📦 Progress {table_name}: {total_pulled} baris...")
 
-            # Update status Progres
-            current_offset += len(df)
-            total_pulled += len(df)
-            print(f"📦 Progress: {total_pulled} baris disimpan ke database...")
-
-        except Exception as e:
-            print(f"DEBUG: [{table_name}] ID: {dataset_id} | Col: {time_col}")
-            print(f"⚠️ Terjadi gangguan: {e}. Mencoba lagi...")
-            # continue # Lanjut lagi kalau error (Self-healing)
-            return
-        
+@flow(name="NYC Taxi Main Pipeline")
 def ingest_all_taxi_data():
+    """Main Flow that regulates all types of taxis"""
     for table_name, info in TARGET_DATASETS.items():
         print(f"===== Processing {table_name} =====")
 
-        ingest_taxi_data(
+        ingest_taxi_data_flow(
             dataset_id=info["dataset_id"],
             table_name=table_name,
             time_col=info["time_column"]
--- src/nyc_taxi/utils/duckdb.py
+++ src/nyc_taxi/utils/duckdb.py
@@ -2,6 +2,7 @@ import duckdb
 import pandas as pd
 from nyc_taxi.config import settings
 import os
+import logging
 
 def get_connection():
     if not os.path.exists("data"):
@@ -11,6 +12,7 @@ def get_connection():
     return duckdb.connect(settings.RAW_DB_PATH)
 
 def save_to_raw(df: pd.DataFrame, table_name: str):
+    logger = logging.getLogger(__name__)
     with get_connection() as conn:
         try:
             # 1. Pastikan Schema & Tabel ada (Auto-commit DDL)
@@ -21,13 +23,13 @@ def save_to_raw(df: pd.DataFrame, table_name: str):
             # Buat tabel kosong jika belum ada
             conn.execute(f"CREATE TABLE IF NOT EXISTS {target_table} AS SELECT * FROM df WHERE 1=0")
             
-            print(f"📥 [DB] Menambahkan {len(df)} baris ke {target_table}...")
+            logger.info(f"📥 [DB] Menambahkan {len(df)} baris ke {target_table}...")
             
             # 2. Gunakan INSERT INTO daripada append untuk kestabilan antar schema
             # DuckDB bisa langsung baca DataFrame 'df' di dalam query SQL
             conn.execute(f"INSERT INTO {target_table} SELECT * FROM df")
             
-            print(f"✅ [DB] {table_name} updated successfully.")
+            logger.info(f"✅ [DB] {table_name} updated successfully.")
             
         except Exception as e:
-            print(f"❌ [DB] Gagal simpan ke DuckDB: {e}")
+            logger.error(f"❌ [DB] Gagal simpan ke DuckDB: {e}")
