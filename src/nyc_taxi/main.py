1d89a2099c4f4d328afa74ca485b946f57e6c490
Claresta <claresta080@gmail.com>
2026-04-03 15:02:42 +0700

    add justfile and fix dataset id for green

--- src/nyc_taxi/main.py
+++ src/nyc_taxi/main.py
@@ -1,7 +1,6 @@
 from nyc_taxi.pipeline.step_01_ingestion.main import ingest_data
 from nyc_taxi.pipeline.step_02_preprocessing.main import preprocess_data
-from nyc_taxi.pipeline.step_03_storage.main import store_data
-from nyc_taxi.pipeline.step_04_analysis.main import run_analysis
+from nyc_taxi.pipeline.step_03_analysis.main import run_analysis
 import logging
 
 def run_pipeline():
@@ -11,8 +10,8 @@ def run_pipeline():
     # Menjalankan urutan tahap demi tahap
     ingest_data()
     preprocess_data()
-    store_data()
     run_analysis()
+    # nanti dilengkapi
     
     logger.info("--- ✅ ALL INTERNAL STAGES COMPLETED ---")
 
