8d44247bb042528b11727247b0163b02789fd98e
Claresta <claresta080@gmail.com>
2026-05-19 13:53:37 +0700

    feat: add temperature and precipitation category

--- src/nyc_taxi/queries/taxi_queries.py
+++ src/nyc_taxi/queries/taxi_queries.py
@@ -9,3 +9,46 @@ class TaxiQueries:
     """
 
     GET_ALL_DATA = "SELECT * FROM {target_table}"
+
+    GET_DATE_TEMP_PREC_YELLOW = "SELECT DATE(tpep_pickup_datetime) as tpep_pickup_datetime, temperature_2m_max, precipitation_sum FROM {target_table}"
+    GET_DATE_TEMP_PREC_GREEN = "SELECT DATE(lpep_pickup_datetime) as lpep_pickup_datetime, temperature_2m_max, precipitation_sum FROM {target_table}"
+
+    ADD_CATEGORY_TEMP = "ALTER TABLE {target_table} ADD COLUMN IF NOT EXISTS temp_category VARCHAR"
+    ADD_CATEGORY_PREC = "ALTER TABLE {target_table} ADD COLUMN IF NOT EXISTS precip_category VARCHAR"
+
+    UPDATE_TEMP_PREC_YELLOW = """
+UPDATE silver.yellow_trips
+SET
+  precip_category =
+    CASE
+      WHEN precipitation_sum = 0 THEN 'tidak hujan'
+      WHEN precipitation_sum <= 2 THEN 'hujan ringan'
+      WHEN precipitation_sum <= 10 THEN 'hujan sedang'
+      ELSE 'hujan lebat'
+    END,
+  temp_category =
+    CASE
+      WHEN temperature_2m_max < 0 THEN 'sangat dingin'
+      WHEN temperature_2m_max < 10 THEN 'dingin'
+      WHEN temperature_2m_max < 20 THEN 'sejuk'
+      ELSE 'hangat'
+    END;
+"""
+    UPDATE_TEMP_PREC_GREEN = """
+UPDATE silver.green_trips
+SET
+  precip_category =
+    CASE
+      WHEN precipitation_sum = 0 THEN 'tidak hujan'
+      WHEN precipitation_sum <= 2 THEN 'hujan ringan'
+      WHEN precipitation_sum <= 10 THEN 'hujan sedang'
+      ELSE 'hujan lebat'
+    END,
+  temp_category =
+    CASE
+      WHEN temperature_2m_max < 0 THEN 'sangat dingin'
+      WHEN temperature_2m_max < 10 THEN 'dingin'
+      WHEN temperature_2m_max < 20 THEN 'sejuk'
+      ELSE 'hangat'
+    END;
+"""
--- src/nyc_taxi/utils/db_utils.py
+++ src/nyc_taxi/utils/db_utils.py
@@ -7,18 +7,21 @@ import streamlit as st
 import pandas as pd
 import json
 import duckdb
-import pugsql
 from pathlib import Path
 
 from nyc_taxi.queries.taxi_queries import TaxiQueries
 
 
 def get_connection():
-    if not os.path.exists(DATA_DIR):
-        os.makedirs(DATA_DIR)
-        print("📁 Folder 'data' berhasil dibuat otomatis!")
-        
-    return duckdb.connect(DATABASE_PATH)
+    os.makedirs(DATA_DIR, exist_ok=True)
+
+    tmp_dir = os.path.join(DATA_DIR, ".duckdb_tmp")
+    os.makedirs(tmp_dir, exist_ok=True)
+
+    conn = duckdb.connect(DATABASE_PATH)
+    conn.execute(f"SET temp_directory='{tmp_dir}'")
+
+    return conn
 
 def save_to_db(df: pd.DataFrame, table_name: str, schema: str):
     logger = logging.getLogger(__name__)
@@ -44,7 +47,7 @@ def save_to_db(df: pd.DataFrame, table_name: str, schema: str):
     except Exception as e:
         logger.error(f"❌ [DB] Gagal simpan ke DuckDB ({target_table}): {e}")
 
-def execute_query(*, query: str, df: pd.DataFrame):
+def execute_query_df(*, query: str, df: pd.DataFrame):
     """General function for run SQL query without returning data"""
     try:
         with get_connection() as conn:
@@ -61,9 +64,11 @@ def query_to_df(query: str) -> pd.DataFrame:
     with get_connection() as conn:
         return conn.execute(query).df()
     
-def execute_query(query: str):
-    """Function for run SELECT and returning Pandas DataFrame"""
-    with get_connection() as conn:
+def execute_query(query: str, conn=None):
+    if conn is None:
+        with get_connection() as c:
+            c.execute(query)
+    else:
         conn.execute(query)
 
 # =========================
